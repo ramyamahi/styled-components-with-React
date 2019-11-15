9091c55928bef5030ea57d66d69b15803cb5bf3e
ramya <ramya.m@spritle.com>
2019-11-15 11:17:41 +0530

    Change styles using props

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const Container = styled.div`
   background-color: black;
   padding: 2%;
   height: 100vh;
+  text-align: center;
 `;
 
 const Header = styled.h1`
@@ -22,11 +23,23 @@ const SubHeader = styled(Header)`
   color: #990000;
 `
 
+const Button = styled.button`
+  text-align: center;
+  color: ${props => props.info ? "white" : "green"};
+  background-color: black;
+  border: 1px solid white;
+  border-radius: 5px;
+  padding: 0.5rem;
+  margin: 1%;
+`; 
+
 function App() {
   return (
     <Container>
       <Header>Welcome to styled component!</Header>
       <SubHeader>Hello</SubHeader>
+      <Button>Start</Button>
+      <Button info>Cancel</Button>
     </Container>
   );
 }
