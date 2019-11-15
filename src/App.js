2393bc47faf3f51f946fbbeef567b0d2c85d079b
ramya <ramya.m@spritle.com>
2019-11-15 10:41:37 +0530

    Simple container, header used styled component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-color: black;
+  padding: 2%;
+  height: 100vh;
+`;
+
+const Header = styled.h1`
+  text-align: center;
+  color: white;
+`; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>Welcome to styled component!
+      </Header>
+    </Container>
   );
 }
 
